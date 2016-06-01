90b1f2fc6861dac1d59c20d6ab8627d3dd7e2cc8
nchronas <nchronas@gmail.com>
2016-06-02 01:54:02 +0300

    added rx, tx for ecss

--- platform/comms/power_ctrl.c
+++ platform/comms/power_ctrl.c
@@ -6,8 +6,8 @@
 /*Must use real pins*/
 SAT_returnState power_control_api(FM_dev_id did, FM_fun_id fid, uint8_t *state) {
 
-    if(!C_ASSERT(did == ADCS_SD_DEV_ID) == true)                             { return SATR_ERROR; }
-    if(!C_ASSERT(fid == P_OFF || fid == P_ON || fid == P_RESET) == true)    { return SATR_ERROR; }
+    if(!C_ASSERT(did == SYS_DBG) == true)                             { return SATR_ERROR; }
+    if(!C_ASSERT(fid == P_OFF || fid == P_ON || fid == P_RESET || fid == SET_VAL) == true)    { return SATR_ERROR; }
 
     if(did == ADCS_SD_DEV_ID && fid == P_ON)         { } //HAL_adcs_SD_ON(); }
     else if(did == ADCS_SD_DEV_ID && fid == P_OFF)   { }//HAL_adcs_SD_OFF(); }
@@ -15,6 +15,10 @@ SAT_returnState power_control_api(FM_dev_id did, FM_fun_id fid, uint8_t *state) 
         //HAL_adcs_SD_OFF();
         HAL_sys_delay(60);
         //HAL_adcs_SD_ON();
+    }
+    else if(did == SYS_DBG && fid == SET_VAL)    {
+
+        HAL_comms_DBG(state[0], state[1]);
     }
 
     return SATR_OK;
