5c937582b5ff6662748c13e2bb819b709b6e7f1d
nchronas <nchronas@gmail.com>
2016-06-02 14:33:48 +0300

    added wod retrieval

--- platform/comms/housekeeping.c
+++ platform/comms/housekeeping.c
@@ -10,8 +10,12 @@ SAT_returnState hk_parameters_report(TC_TM_app_id app_id, HK_struct_id sid, uint
 SAT_returnState hk_report_parameters(HK_struct_id sid, tc_tm_pkt *pkt) {
     
     pkt->data[0] = (HK_struct_id)sid;
-    
-    if(sid == EX_HEALTH_REP) {
+
+	if(sid == HEALTH_REP) {
+        pkt->data[1] = (uint8_t)(get_temp_adt7420());
+
+        pkt->len = 2;
+    } else if(sid == EX_HEALTH_REP) {
 
         //cnv.cnv32 = time.now();
         cnv32_8(time_now(), &pkt->data[1]);
