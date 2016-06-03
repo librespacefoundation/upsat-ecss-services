b987e9a2b73b0702999865e81b25fc9f4e4637f3
nchronas <nchronas@gmail.com>
2016-06-03 15:51:02 +0300

    fixed housekeeping issue

--- platform/comms/housekeeping.c
+++ platform/comms/housekeeping.c
@@ -5,6 +5,11 @@
 #define __FILE_ID__ 666
 
 SAT_returnState hk_parameters_report(TC_TM_app_id app_id, HK_struct_id sid, uint8_t *data) {
+
+   if(sid == WOD_REP) {
+
+   }
+
    return SATR_ERROR;
 }
 
@@ -22,11 +27,7 @@ SAT_returnState hk_report_parameters(HK_struct_id sid, tc_tm_pkt *pkt) {
         cnv32_8(HAL_sys_GetTick(), &pkt->data[1]);
         pkt->len = 5;
 
-    } else if(sid == WOD_REP) {
-
-
-
-    }
+    } 
 
     return SATR_OK;
 }
