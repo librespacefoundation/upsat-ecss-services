2208f0fb9c115996b618b89eeca3b16bd4ed722f
Manolis Surligas <surligas@csd.uoc.gr>
2016-06-14 18:22:31 +0300

    Fix cyclic dependency on header files.
    With this fix the APP IDs are properly declared when the "system.h"
    header file is handled by the preprocessor.

---
+++ services/subsystems_ids.h
@@ -0,0 +1,19 @@
+
+#ifndef SERVICES_SUBSYSTEMS_IDS_H_
+#define SERVICES_SUBSYSTEMS_IDS_H_
+
+/**
+ * Each subsystem has a unique APP ID that is used to route ECSS packets
+ * within the different satellite subsystems.
+ */
+
+#define _OBC_APP_ID_   1
+#define _EPS_APP_ID_   2
+#define _ADCS_APP_ID_  3
+#define _COMMS_APP_ID_ 4
+#define _IAC_APP_ID_   5
+#define _GND_APP_ID_   6
+#define _DBG_APP_ID_   7
+#define _LAST_APP_ID_  8
+
+#endif /* SERVICES_SUBSYSTEMS_IDS_H_ */
