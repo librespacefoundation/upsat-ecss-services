bb280e1d9997e413f7c6c443dc6af8b7d1d8af50
nchronas <nchronas@gmail.com>
2016-07-12 13:26:27 +0300

    Fixed sram pointer to point to correct locations

--- platform/obc/obc.c
+++ platform/obc/obc.c
@@ -126,37 +126,72 @@ SAT_returnState obc_INIT() {
 
 void bkup_sram_INIT() {
 
-    obc_data.log_cnt = HAL_obc_BKPSRAM_BASE();
-    obc_data.log_state = HAL_obc_BKPSRAM_BASE() + 1;
-    sys_data.boot_counter = HAL_obc_BKPSRAM_BASE() + 2;
-    obc_data.wod_cnt = HAL_obc_BKPSRAM_BASE() + 3;
+    uint8_t *base_pointer = (uint8_t*)HAL_obc_BKPSRAM_BASE();
 
-    obc_data.fs_su_head = (uint16_t*)HAL_obc_BKPSRAM_BASE() + 4;
-    obc_data.fs_wod_head = (uint16_t*)HAL_obc_BKPSRAM_BASE() + 5;
-    obc_data.fs_ext_head = (uint16_t*)HAL_obc_BKPSRAM_BASE() + 6;
-    obc_data.fs_ev_head = (uint16_t*)HAL_obc_BKPSRAM_BASE() + 7;
+    obc_data.log_cnt = (uint32_t*)base_pointer;
+    base_pointer += sizeof(uint32_t);
 
-    obc_data.fs_su_tail = (uint16_t*)HAL_obc_BKPSRAM_BASE() + 8;
-    obc_data.fs_wod_tail = (uint16_t*)HAL_obc_BKPSRAM_BASE() + 9;
-    obc_data.fs_ext_tail = (uint16_t*)HAL_obc_BKPSRAM_BASE() + 10;
-    obc_data.fs_ev_tail = (uint16_t*)HAL_obc_BKPSRAM_BASE() + 11;
+    obc_data.log_state = (uint32_t*)base_pointer;
+    base_pointer += sizeof(uint32_t);
 
-    obc_data.fs_fotos = (uint16_t*)HAL_obc_BKPSRAM_BASE() + 12;
+    sys_data.boot_counter = (uint32_t*)base_pointer;
+    base_pointer += sizeof(uint32_t);
+
+    obc_data.wod_cnt = (uint32_t*)base_pointer;
+    base_pointer += sizeof(uint32_t);
+
+    MNLP_data.su_nmlp_perm_state_pnt = (uint32_t *) base_pointer; //264; //265;
+    base_pointer += sizeof(uint32_t);
+
+    obc_data.fs_su_head = (uint16_t*)base_pointer;
+    base_pointer += sizeof(uint16_t);
+
+    obc_data.fs_wod_head = (uint16_t*)base_pointer;
+    base_pointer += sizeof(uint16_t);
+
+    obc_data.fs_ext_head = (uint16_t*)base_pointer;
+    base_pointer += sizeof(uint16_t);
+
+    obc_data.fs_ev_head = (uint16_t*)base_pointer;
+    base_pointer += sizeof(uint16_t);
+
+    obc_data.fs_su_tail = (uint16_t*)base_pointer;
+    base_pointer += sizeof(uint16_t);
+
+    obc_data.fs_wod_tail = (uint16_t*)base_pointer;
+    base_pointer += sizeof(uint16_t);
+
+    obc_data.fs_ext_tail = (uint16_t*)base_pointer;
+    base_pointer += sizeof(uint16_t);
+
+    obc_data.fs_ev_tail = (uint16_t*)base_pointer;
+    base_pointer += sizeof(uint16_t);
+
+    obc_data.fs_fotos = (uint16_t*)base_pointer;
+    base_pointer += sizeof(uint16_t);
     
-    MNLP_data.su_nmlp_scheduler_active =  (uint8_t*) HAL_obc_BKPSRAM_BASE()+13;
+    MNLP_data.su_nmlp_scheduler_active =  (uint8_t*) base_pointer;
+    base_pointer += sizeof(uint8_t);
+
     //*MNLP_data.su_nmlp_scheduler_active = (uint8_t) false;
-    MNLP_data.su_nmlp_last_active_script = (uint8_t*) HAL_obc_BKPSRAM_BASE()+14;
+    MNLP_data.su_nmlp_last_active_script = (uint8_t*) base_pointer;
+    base_pointer += sizeof(uint8_t);
+
     //*MNLP_data.su_nmlp_last_active_script = 0;
-    MNLP_data.su_next_time_table = (uint8_t*) HAL_obc_BKPSRAM_BASE()+15;
+    MNLP_data.su_next_time_table = (uint8_t*) base_pointer;
+    base_pointer += sizeof(uint8_t);
 //    *MNLP_data.su_next_time_table = 0;
-    MNLP_data.su_next_script_seq = (uint8_t*) HAL_obc_BKPSRAM_BASE()+16;
+    MNLP_data.su_next_script_seq = (uint8_t*) base_pointer;
+    base_pointer += sizeof(uint8_t);
 //    *MNLP_data.su_next_script_seq = 0;
-    MNLP_data.su_nmlp_perm_state_pnt = (uint32_t *) HAL_obc_BKPSRAM_BASE()+17; //264; //265;
+
     //*MNLP_data.su_nmlp_perm_state_pnt = 0;
     
-    obc_data.log = (uint8_t *)HAL_obc_BKPSRAM_BASE() + 18;
-    obc_data.wod_log = (uint8_t *)HAL_obc_BKPSRAM_BASE() + 19 + (EV_MAX_BUFFER);
-    
+    obc_data.log = (uint8_t *)base_pointer;
+    base_pointer += sizeof(uint8_t);
+    obc_data.wod_log = (uint8_t *)base_pointer + (EV_MAX_BUFFER);
+    base_pointer += sizeof(uint8_t);
+
     if(!C_ASSERT(*obc_data.log_cnt < EV_MAX_BUFFER) == true)      { *obc_data.log_cnt = 0; }
     if(!C_ASSERT(*obc_data.wod_cnt < EV_MAX_BUFFER) == true)      { *obc_data.wod_cnt = 0; }
     if(!C_ASSERT(*obc_data.fs_su_head < MS_MAX_FILES) == true)    { *obc_data.fs_su_head = 1; }
