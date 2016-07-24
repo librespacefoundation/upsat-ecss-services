d11fbe696bfbbd17a548adc96947d72fbf5f541b
nchronas <nchronas@gmail.com>
2016-07-24 05:06:30 +0300

    Typecast system definition to enum, added consts

--- services/housekeeping_service.h
+++ services/housekeeping_service.h
@@ -6,10 +6,10 @@
 
 SAT_returnState hk_app(tc_tm_pkt *pkt);
 
-SAT_returnState hk_crt_pkt_TC(tc_tm_pkt *pkt, TC_TM_app_id app_id, HK_struct_id sid);
+SAT_returnState hk_crt_pkt_TC(tc_tm_pkt *pkt, const TC_TM_app_id app_id, const HK_struct_id sid);
 
-SAT_returnState hk_crt_pkt_TM(tc_tm_pkt *pkt, TC_TM_app_id app_id, HK_struct_id sid);
+SAT_returnState hk_crt_pkt_TM(tc_tm_pkt *pkt, const TC_TM_app_id app_id, const HK_struct_id sid);
 
-SAT_returnState hk_crt_empty_pkt_TM(tc_tm_pkt **pkt, TC_TM_app_id app_id, HK_struct_id sid);
+SAT_returnState hk_crt_empty_pkt_TM(tc_tm_pkt **pkt, const TC_TM_app_id app_id, const HK_struct_id sid);
 
 #endif
