f87d17b02f893b3764b4b57ab9ea5d0ce5633242
nchronas <nchronas@gmail.com>
2016-07-31 18:32:15 +0300

    Added functionality for byte set

--- platform/comms/power_ctrl.c
+++ platform/comms/power_ctrl.c
@@ -6,15 +6,17 @@
 /*Must use real pins*/
 SAT_returnState power_control_api(FM_dev_id did, FM_fun_id fid, uint8_t *state) {
 
-    if(!C_ASSERT(did == SYS_DBG) == true)                             { return SATR_ERROR; }
-    if(!C_ASSERT(fid == P_OFF || fid == P_ON || fid == P_RESET || fid == SET_VAL) == true)    { return SATR_ERROR; }
+    if(!C_ASSERT(did == COMMS_WOD_PAT ||
+                 did == SYS_DBG ) == true)   { return SATR_ERROR; }
 
-    if(did == ADCS_SD_DEV_ID && fid == P_ON)         { } //HAL_adcs_SD_ON(); }
-    else if(did == ADCS_SD_DEV_ID && fid == P_OFF)   { }//HAL_adcs_SD_OFF(); }
-    else if(did == ADCS_SD_DEV_ID && fid == P_RESET) {
-        //HAL_adcs_SD_OFF();
-        HAL_sys_delay(60);
-        //HAL_adcs_SD_ON();
+    if(!C_ASSERT(fid == P_OFF ||
+                 fid == P_ON ||
+                 fid == P_RESET ||
+                 fid == SET_VAL) == true)    { return SATR_ERROR; }
+
+    if (did == COMMS_WOD_PAT && fid == SET_VAL) {
+
+        pattern = state[0];
     }
     else if(did == SYS_DBG && fid == SET_VAL)    {
       //FIXME
