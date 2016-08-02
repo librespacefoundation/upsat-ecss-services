e1f8014502925bd484690ff0dd8a03b800f9d5b7
Manolis Surligas <surligas@gmail.com>
2016-08-02 12:13:54 +0300

    Change the headless beacon TX pattern with proper command

--- platform/comms/power_ctrl.c
+++ platform/comms/power_ctrl.c
@@ -1,4 +1,6 @@
 #include "power_ctrl.h"
+#include "config.h"
+#include "persistent_mem.h"
 
 #undef __FILE_ID__
 #define __FILE_ID__ 23
@@ -6,15 +8,17 @@
 /*Must use real pins*/
 SAT_returnState power_control_api(FM_dev_id did, FM_fun_id fid, uint8_t *state) {
 
-    if(!C_ASSERT(did == SYS_DBG) == true)                             { return SATR_ERROR; }
-    if(!C_ASSERT(fid == P_OFF || fid == P_ON || fid == P_RESET || fid == SET_VAL) == true)    { return SATR_ERROR; }
+    if(!C_ASSERT(did == COMMS_WOD_PAT || did == SYS_DBG) == true) {
+      return SATR_ERROR;
+    }
+    if (!C_ASSERT(fid == P_OFF || fid == P_ON || fid == P_RESET
+		  || fid == SET_VAL) == true) {
+      return SATR_ERROR;
+    }
 
-    if(did == ADCS_SD_DEV_ID && fid == P_ON)         { } //HAL_adcs_SD_ON(); }
-    else if(did == ADCS_SD_DEV_ID && fid == P_OFF)   { }//HAL_adcs_SD_OFF(); }
-    else if(did == ADCS_SD_DEV_ID && fid == P_RESET) {
-        //HAL_adcs_SD_OFF();
-        HAL_sys_delay(60);
-        //HAL_adcs_SD_ON();
+    if (did == COMMS_WOD_PAT && fid == SET_VAL) {
+      comms_write_persistent_word(state[0],
+				  __COMMS_HEADLESS_TX_FLASH_OFFSET);
     }
     else if(did == SYS_DBG && fid == SET_VAL)    {
       //FIXME
