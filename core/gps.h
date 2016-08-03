24eccc13b27c06849e8ba80818c9ea32974507c7
zisi <azisi@windowslive.com>
2016-08-03 14:12:11 +0300

    Updates in GPS functions.
    Change types and names in variables.
    Change argument of functions (2d array) according to C99.
    Fix the fill of res[][] array.

--- core/gps.h
+++ core/gps.h
@@ -5,12 +5,14 @@
 #include <stdint.h>
 #include <string.h>
 #include "services.h"
+#include "adcs_frame.h"
 
 #define NMEA_MAX_FIELD_SIZE 15
-#define NMEA_MAX_FIELDS     20
+#define NMEA_MAX_FIELDS     22
 #define NMEA_HEADER         0
 #define NMEA_HEADER_SIZE    6
 #define NMEA_MAX_LEN        80
+#define NMEA_NUM_SENTENCES  8
 
 #define NMEA_GSA_3DFIX      2
 
@@ -28,24 +30,25 @@
 #define NMEA_GGA_TIME       1
 #define NMEA_GGA_NUM_SAT    7
 
+typedef enum {
+    GPS_ERROR_FLASH = 0, GPS_ERROR_HAL, GPS_ERROR, GPS_OFF, GPS_UNLOCK, GPS_RESET
+} _gps_status;
+
 typedef struct {
     uint8_t d3fix;
-    float x_wgs;
-    float y_wgs;
-    float z_wgs;
-    float vx_wgs;
-    float vy_wgs;
-    float vz_wgs;
-    float time;
-    double gps_time;
-    uint16_t gps_week;
+    xyz_t p_gps_ecef; // in km
+    xyz_t v_gps_ecef; // in km/s
+    float utc_time;
+    double sec;
+    uint16_t week;
     uint8_t num_sat;
+    _gps_status status;
 } _gps_state;
 
-extern _gps_state gps;
+extern _gps_state gps_state;
 
-SAT_returnState gps_parse_fields(uint8_t *buf, const uint8_t size,  uint8_t (*res)[NMEA_MAX_FIELDS][NMEA_MAX_FIELD_SIZE]);
+SAT_returnState gps_parse_fields(uint8_t *buf, const uint8_t size,  uint8_t (*res)[NMEA_MAX_FIELDS]);
 
-SAT_returnState gps_parse_logic(const uint8_t (*res)[NMEA_MAX_FIELDS][NMEA_MAX_FIELD_SIZE], _gps_state *gps_state);
+SAT_returnState gps_parse_logic(const uint8_t (*res)[NMEA_MAX_FIELDS], _gps_state *state);
 
 #endif
