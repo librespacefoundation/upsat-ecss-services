269fee63beb44456460f0fbccba13556219f0cf6
zisi <azisi@windowslive.com>
2016-08-07 22:58:14 +0300

    Add GPS ON-OFF functionality.

--- platform/adcs/power_ctrl.c
+++ platform/adcs/power_ctrl.c
@@ -4,9 +4,7 @@
 
 #include "sysview.h"
 
-#include "sgp4.h"
-#include "adcs_control.h"
-#include "adcs_error_handler.h"
+#include "adcs_manager.h"
 
 #undef __FILE_ID__
 #define __FILE_ID__ 27
@@ -46,30 +44,41 @@ SAT_returnState power_control_api(FM_dev_id did, FM_fun_id fid, uint8_t *state) 
 
     else if(did == ADCS_GPS && fid == P_ON) {
         HAL_adcs_GPS_ON();
+        struct time_utc gps_utc_on_gnd;
+        get_time_UTC(&gps_utc_on_gnd);
+        gps_state.status = HAL_SetAlarm_GPS_LOCK(gps_utc_on_gnd, GPS_ALARM_UNLOCK);
+        if (gps_state.status == GPS_ERROR_FLASH) {
+            gps_state.status = GPS_UNLOCK;
+            error_status = ERROR_FLASH;
+        }
         SYSVIEW_PRINT("SET GPS ON");
     }
     else if(did == ADCS_GPS && fid == P_OFF) {
         HAL_adcs_GPS_OFF();
+        gps_state.status = GPS_OFF;
         SYSVIEW_PRINT("SET GPS OFF");
     }
 
     else if(did == ADCS_SPIN && fid == SET_VAL)    {
-        SYSVIEW_PRINT("SET SPIN");
         int32_t rpm = 0;
         cnv8_32(state, &rpm);
         HAL_adcs_SPIN(rpm); 
+        SYSVIEW_PRINT("SPIN RPM %ld", rpm);
     }
     else if(did == ADCS_MAGNETO && fid == SET_VAL)    {
         SYSVIEW_PRINT("SET MAGNETO");
-        int8_t current_x = 0;
+        int8_t current_z = 0;
         int8_t current_y = 0;
 
-        current_x = state[0];
+        current_z = state[0];
         current_y = state[1];
-        HAL_adcs_MAGNETO (current_x, current_y);
+        HAL_adcs_MAGNETO (current_z, current_y);
+        SYSVIEW_PRINT("CURRENT %d %d", current_z, current_y);
     }
     else if (did == ADCS_TLE && fid == SET_VAL) {
         SYSVIEW_PRINT("SET TLE");
+        uint8_t tle_string[TLE_SIZE];
+        memset(tle_string, 0, TLE_SIZE);
         for (uint8_t i = 1; i < TLE_SIZE + 1; i++) {
             tle_string[i] = state[i];
         }
@@ -86,13 +95,13 @@ SAT_returnState power_control_api(FM_dev_id did, FM_fun_id fid, uint8_t *state) 
         cnv8_16(&state[0], &control.gain[0]);
         cnv8_16(&state[2], &control.gain[1]);
         cnv8_16(&state[4], &control.gain[2]);
-        SYSVIEW_PRINT("SET GAINS");
+        SYSVIEW_PRINT("GAINS %d %d %d", control.gain[0], control.gain[1], control.gain[2]);
     }
     else if(did == ADCS_SET_POINT && fid == SET_VAL) {
         cnv8_16(&state[0], &control.set_point[0]);
         cnv8_16(&state[2], &control.set_point[1]);
         cnv8_16(&state[4], &control.set_point[2]);
-        SYSVIEW_PRINT("SET POINT");
+        SYSVIEW_PRINT("SET POINTS %d %d %d", control.set_point[0], control.set_point[1], control.set_point[2]);
     }
     else if(did == SYS_DBG && fid == SET_VAL)    {
         HAL_adcs_DBG(state[0], state[1]);
