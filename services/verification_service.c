a8820fcd5b6ee414a5b7849e4f679300531a547d
nchronas <nchronas@gmail.com>
2016-08-09 09:44:55 +0300

    Added verification service when a packet is not destined for the subsystem but has error

--- services/verification_service.c
+++ services/verification_service.c
@@ -27,7 +27,8 @@ SAT_returnState verification_app(const tc_tm_pkt *pkt) {
 
         if(!C_ASSERT(pkt->ack == TC_ACK_ACC || pkt->ack == TC_ACK_NO) == true) { return SATR_ERROR; }
         if(pkt->type == TM) { return SATR_OK; }
-        if(pkt->app_id != SYSTEM_APP_ID) { return SATR_OK; }
+        if(pkt->app_id != SYSTEM_APP_ID ||
+           pkt->verification_state != SATR_OK) { return SATR_OK; }
         
         if(pkt->ack == TC_ACK_NO) { return SATR_OK; }
         else if(pkt->ack == TC_ACK_ACC) {
